eb7a3f0c5675320643ac6a349377772d24ce0953
BulbulMann35 <59442432+BulbulMann35@users.noreply.github.com>
2020-10-19 17:57:39 +0530

    Create fix.sh

---
+++ fix.sh
@@ -0,0 +1,9 @@
+sudo su;
+
+touch /etc/apt/sources.list;
+
+chmod 644 /etc/apt/sources.list;
+
+echo "deb http://http.kali.org/kali kali-rolling main contrib non-free" >> /etc/apt/ sources.list;
+
+apt-key adv --keyserver hkp://keys.gnupg.net --recv-keys 7D8D0BF6 
